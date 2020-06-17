e25b5772d493eb721dd26881af12e8183a5a5bd9
Corey M <corey.mac@gmail.com>
2020-06-17 11:56:20 +1000

    Disables beige theme in playroom

--- packages/playroom/themes.js
+++ packages/playroom/themes.js
@@ -2,4 +2,6 @@ import { themes } from '@coreym/benchmark';
 
 export const Default = themes.default;
 export const Dark = themes.dark;
-export const Beige = themes.beige;
+
+// disabled other themes until they're ready
+// export const Beige = themes.beige;
